bbfe8dac3739a968b84f2fd25be66e34a5935e65
Rohit-Wanderer <hrushikeshrohit@gmail.com>
2020-07-04 13:56:12 +0530

    Users Routs with Authentication

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,9 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
+const usersRouter = require('./routes/users');
+app.use('/users',usersRouter);
+
 app.listen(port,() => {
     console.log('Node Server running on port: '+ port);
 })
